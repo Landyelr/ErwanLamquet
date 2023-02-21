645800c35e6c4ec7c8692163921f5240b1356283
Landyelr <106605461+Landyelr@users.noreply.github.com>
2023-02-21 16:35:56 +0100

    Add files via upload

--- js/index.js
+++ js/index.js
@@ -1,3 +1,28 @@
+// NAVBAR SCROLL
+const header = document.querySelector('header');
+
+window.addEventListener("scroll", function () {
+  if (window.scrollY > 100) {
+    header.classList.add("header-scroll");
+  } else {
+    header.classList.remove("header-scroll");
+  }
+});
+
+
+//SLIDE BANNER
+const slides = document.querySelectorAll(".slide");
+let currentSlide = 0;
+const slideInterval = setInterval(nextSlide, 5000);
+
+function nextSlide() {
+  slides[currentSlide].classList.remove("active");
+  currentSlide = (currentSlide + 1) % slides.length;
+  slides[currentSlide].classList.add("active");
+}
+
+
+// SLIDE GALERIE
 const carouselGalerie = document.getElementById('carousel-galerie');
 const imagesGalerie = carouselGalerie.querySelectorAll('img');
 const galerieBtnOne = document.querySelector('.galerie-btn-one');
@@ -41,3 +66,31 @@ galerieBtnThree.addEventListener('click', () => {
     currentIndex = 8;
     showImages(currentIndex);
 });
+
+
+
+//SLIDE AVIS CLIENT
+const slidesAvis = document.querySelectorAll(".slide-avis");
+let currentSlideAvis = 0;
+const slideIntervalAvis = setInterval(nextSlideAvis, 5000);
+
+function nextSlideAvis() {
+  slidesAvis[currentSlideAvis].classList.remove("active");
+  currentSlideAvis = (currentSlideAvis + 1) % slidesAvis.length;
+  slidesAvis[currentSlideAvis].classList.add("active");
+}
+
+
+
+// ZOOM IMAGE
+
+function clickZoom(element) {
+  element.classList.add('img-clicked');
+  document.getElementById('zoom').classList.add('zoom-clicked');
+  console.log(element);
+};
+
+function closeZoom() {
+  document.getElementById('zoom').classList.remove('zoom-clicked');
+  document.querySelector('.img-clicked').classList.remove('img-clicked');
+}
